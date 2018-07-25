c8a26c7299c78fc9585c77da8408181cf53af654
huangyang <huangyang3@asiainfo.com>
2018-07-25 18:21:10 +0800

    验证码生成算法1.0。

---
+++ src/main/java/com/asiainfo/boot/controller/MainClass.java
@@ -0,0 +1,50 @@
+package com.asiainfo.boot.controller;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * 项目名称:   pinkstone
+ * 包:        com.asiainfo.boot.controller
+ * 类名称:     MainClass
+ * 类描述:     main clas
+ * @author :   huangyang
+ * 创建时间:   2018/7/25 15:06
+ */
+public class MainClass {
+
+
+	public static void main(String[] args) {
+
+		System.out.println(getSms(8));
+
+	}
+
+	/**
+	 * 获取不重复的短信验证码
+	 *
+	 * @param length 验证码位数
+	 * @return
+	 */
+	static String getSms(int length) {
+
+		Set<Character> chars = new HashSet<>();
+		Random random = new Random();
+		for (int i = 0; i < i + 1; i++) {
+			if (chars.size() == length) {
+				break;
+			} else {
+				if (i % 2 == 0) {
+					chars.add(String.valueOf(random.nextInt(10)).toCharArray()[0]);
+				} else {
+					int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;
+					chars.add((char) (random.nextInt(26) + temp));
+				}
+			}
+		}
+		return StringUtils.join(chars.toArray());
+	}
+}
